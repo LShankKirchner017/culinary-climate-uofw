40b96ac3da9282b07dd4b5268f628d19ef6b410e
cumauu73 <cumauu73@icloud.com>
2023-07-17 18:06:38 -0500

    added click listener to button

--- assets/script.js
+++ assets/script.js
@@ -1,33 +1,49 @@
-var apiKey = '312ef17758b755a8564935f0cd1d338b'; // Replace with your actual API key from OpenWeatherMap
+// Add event listener to the button
+document.getElementById('fetchButton').addEventListener('click', fetchWeather);
 
-async function fetchWeatherDataByCityName(cityName) {
-  const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURIComponent(cityName)}&appid=${apiKey}&units=metric`;
+function fetchWeather() {
+  var apiKey = '312ef17758b755a8564935f0cd1d338b';
+  var city = document.getElementById('cityInput').value;
+  var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=imperial`;
 
-  try {
-    const response = await fetch(apiUrl);
-    const data = await response.json();
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      
+      var temperature = data.main.temp;
+      var humidity = data.main.humidity;
+      var description = data.weather[0].description;
+      var iconCode = data.weather[0].icon;
 
-    if (response.ok) {
-      // Weather data fetched successfully
-      return data;
-    } else {
-      // Handle API error response
-      throw new Error(data.message);
-    }
-  } catch (error) {
-    // Handle fetch or JSON parsing errors
-    console.error('Error:', error.message);
-  }
+      
+      var card = document.createElement('div');
+      card.classList.add('card');
+
+      var temperatureElement = document.createElement('p');
+      temperatureElement.textContent = `Temperature: ${temperature}°C`;
+
+      var humidityElement = document.createElement('p');
+      humidityElement.textContent = `Humidity: ${humidity}%`;
+
+      var descriptionElement = document.createElement('p');
+      descriptionElement.textContent = `Description: ${description}`;
+
+      var iconElement = document.createElement('img');
+      iconElement.src = `https://openweathermap.org/img/w/${iconCode}.png`;
+      iconElement.alt = 'Weather Icon';
+
+      // Append the weather data elements to the card
+      card.appendChild(temperatureElement);
+      card.appendChild(humidityElement);
+      card.appendChild(descriptionElement);
+      card.appendChild(iconElement);
+
+      // Display the card in the weather container
+      const weatherContainer = document.getElementById('weatherContainer');
+      weatherContainer.innerHTML = ''; // Clear previous results
+      weatherContainer.appendChild(card);
+    })
+    .catch(error => {
+      console.log('Error fetching weather data:', error);
+    });
 }
-
-// Usage example:
-const city = 'London'; // Replace with the desired city name
-fetchWeatherDataByCityName(city)
-  .then(data => {
-    // Process the weather data
-    console.log(data);
-  })
-  .catch(error => {
-    // Handle any errors
-    console.error(error);
-  });
